8f7b5060853d1fcf7db09ef756785c91d19fc6e7
Antoine <al@live.be>
2020-07-02 17:50:21 +0200

    Initial commit

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,30 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext extends \Behat\MinkExtension\Context\MinkContext implements Context
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Then I wait during :arg1 ms
+     */
+    public function iWaitDuringMs($arg1)
+    {
+        $this->getSession()->wait($arg1);
+    }
+}
